02648bd26de478c8ac2c4213eeacc941995413c5
Darwin Jore <>
2018-02-28 18:42:11 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'darwin',
-        text: 'Hi there.'
-    });
 });
 
 socket.on('disconnect', function () {
